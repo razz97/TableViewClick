31231fe44b7d2f6cb1dcfb77bb45043dcd30cddc
Alex Bou <alu2017445@aaaaaa.local>
2019-01-16 19:56:18 +0100

    project created

---
+++ TableViewClick/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewClick
+//
+//  Created by Alex Bou on 16/01/2019.
+//  Copyright © 2019 Alex Bou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
